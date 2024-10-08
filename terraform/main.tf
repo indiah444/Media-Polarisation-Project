9e48980f0f1fc3791df0a623ed33a9124b835004
Shayak Hussain <shayak201@gmail.com>
2024-10-08 14:31:01 +0100

    sample images

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,10 +25,10 @@ data "aws_ecr_image" "article_combiner_image" {
     image_tag       = "latest"
 }
 
-data "aws_ecr_image" "email_generator_image" {
-    repository_name = "c13-boudicca-mp-email-generator"
-    image_tag       = "latest"
-}
+# data "aws_ecr_image" "email_generator_image" {
+#     repository_name = "c13-boudicca-mp-email-generator"
+#     image_tag       = "latest"
+# }
 
 data "aws_iam_role" "execution_role" { name = "ecsTaskExecutionRole" }
 data "aws_ecs_cluster" "c13_cluster" { cluster_name = "c13-ecs-cluster" }
@@ -68,7 +68,7 @@ resource "aws_lambda_function" "fox_news_scraper_lambda" {
 }
 
 
-resource "aws_lambda_function" "democracy_news_now_scraper_lambda" {
+resource "aws_lambda_function" "democracy_now_news_scraper_lambda" {
     function_name = "c13-boudicca-mp-democracy-now-news-scraper"
     image_uri     = data.aws_ecr_image.democracy_now_news_scraper_image.image_uri
     role          = aws_iam_role.lambda_execution_role.arn
@@ -92,13 +92,125 @@ resource "aws_lambda_function" "article_combiner_lambda" {
     image_config { command = ["main.lambda_handler"] }
 }
 
-resource "aws_lambda_function" "email_generator_lambda" {
-    function_name = "c13-boudicca-mp-email-generator"
-    image_uri     = data.aws_ecr_image.email_generator_image.image_uri
-    role          = aws_iam_role.lambda_execution_role.arn
-    package_type = "Image"
-    timeout = 900
-    memory_size = 256
+# resource "aws_lambda_function" "email_generator_lambda" {
+#     function_name = "c13-boudicca-mp-email-generator"
+#     image_uri     = data.aws_ecr_image.email_generator_image.image_uri
+#     role          = aws_iam_role.lambda_execution_role.arn
+#     package_type = "Image"
+#     timeout = 900
+#     memory_size = 256
 
-    image_config { command = ["main.lambda_handler"] }
+#     image_config { command = ["main.lambda_handler"] }
+# }
+
+resource "aws_iam_role" "step_functions_role" {
+  name = "c13-boudicca-mp-main-pipeline-step-functions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "states.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "step_functions_policy" {
+  name = "c13-boudicca-mp-main-pipeline-functions-policy"
+  role = aws_iam_role.step_functions_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = ["lambda:InvokeFunction"],
+        Resource = [
+          aws_lambda_function.fox_news_scraper_lambda.arn,
+          aws_lambda_function.democracy_now_news_scraper_lambda.arn,
+          aws_lambda_function.article_combiner_lambda.arn
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_sfn_state_machine" "main_pipeline_state_machine" {
+  name     = "c13-boudicca-mp-main-pipeline-state-machine"
+  role_arn = aws_iam_role.step_functions_role.arn
+
+  definition = jsonencode({
+    StartAt = "SummaryReportTask",
+    States = {
+      FoxScraperTask = {
+        Type = "Task",
+        Resource = aws_lambda_function.fox_news_scraper_lambda.arn,
+        ResultPath: "$.reportResult",
+        Next = "SendEmailTask"
+      },
+      DemocracyNowScraperTask = {
+        Type = "Task",
+        Resource = aws_lambda_function.democracy_now_news_scraper_lambda.arn,
+        ResultPath: "$.reportResult",
+        Next = "SendEmailTask"
+      },
+      ArticleCombineTask = {
+        Type = "Task",
+        Resource = aws_lambda_function.article_combiner_lambda.arn,
+        ResultPath: "$.reportResult",
+        Next = "SendEmailTask"
+      }
+    }
+  })
+}
+
+data "aws_iam_policy_document" "step_functions_schedule_trust_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["scheduler.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "step_functions_scheduler_role" {
+  name               = "c13-boudicca-mp-main-pipeline-step-functions-scheduler-role"
+  assume_role_policy = data.aws_iam_policy_document.step_functions_schedule_trust_policy.json
+}
+
+resource "aws_iam_role_policy" "step_functions_scheduler_policy" {
+  name = "c13-boudicca-mp-main-pipeline-step-functions-scheduler-policy"
+  role = aws_iam_role.step_functions_scheduler_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "states:StartExecution",
+        Resource = aws_sfn_state_machine.main_pipeline_state_machine.arn
+      }
+    ]
+  })
+}
+
+resource "aws_scheduler_schedule" "daily_step_function_trigger" {
+  name                       = "c13-boudicca-mp-main-pipeline-schedule"
+  schedule_expression        = "cron(0 */1 * * ? *)"
+  schedule_expression_timezone = "UTC+01:00"
+  flexible_time_window {
+    mode = "OFF"
+  }
+  target {
+    arn     = aws_sfn_state_machine.main_pipeline_state_machine.arn
+    role_arn = aws_iam_role.step_functions_scheduler_role.arn
+    input   = jsonencode({})
+  }
 }
