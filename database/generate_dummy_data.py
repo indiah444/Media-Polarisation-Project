40059bbfbfcec83b9f57deba1126592ed93cd45a
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-08 16:11:58 +0100

    fixed source_id typo

--- database/generate_dummy_data.py
+++ database/generate_dummy_data.py
@@ -7,46 +7,28 @@ from dotenv import load_dotenv
 import psycopg2
 from psycopg2 import extras
 
-TOPICS = ["Immigration", "Justice system", "P Diddy",
-          "Supreme Court decisions", "Religion",
-          "Natural disasters", "Hurricane Helene", "World news",
-          "Climate change", "China", "Education", "Woke activism",
-            "Crime and law enforcement", "Guns", "Economics",
-          "Recession", "Inflation", "Cost of living", "Politics",
-          "Trump", "Kamala", "Voting", "Tim Walz", "JD Vance",
-          "War", "Israel-Gaza", "Russia-Ukraine", "Iran", "Lebanon",
-          "Social", "Race relations", "LGBTQ+ issues", "Abortion"
+TOPICS = ["Trump", 
+          "Kamala", 
+          "Israel-Gaza",
+          "Hurricanes", 
+          "Natural disasters",
+          "World news",
+          "Climate change", 
+          "Voting",
           ]
 
 
 FOX_HEADLINES = {
-    "Mexican mayor's severed head placed atop pick-up truck 6 days after taking office": 
-    ["World news", "Immigration"],
-
     "Supreme Court denies Biden administration appeal over federal emergency abortion requirement in Texas":
-
     ["Supreme Court decisions", "Justice system"],
 
-    "P Diddy sued by former employee over workplace harassment": 
-    ["P Diddy", "Justice system"],
-
     "Massive wildfires rage across California amid intense heatwave, thousands evacuated":
-
-    ["Natural disasters", "Climate change"],
+    ["Natural disasters"],
 
     "Kamala Harris faces backlash after controversial comments on immigration policies":
-
-    ["Politics", "Kamala", "Immigration"],
-
-    "China's new military drills raise tensions in the South China Sea": 
-    ["China", "World news"],
-
-    "Justice Department investigates police handling of race-related protests in Minneapolis":
-
-    ["Justice system", "Race relations", "Crime and law enforcement"],
+    ["Kamala"],
 
     "US inflation hits 40-year high, causing concerns for the economy":
-
     ["Economics", "Inflation", "Cost of living"],
 
     "Russia-Ukraine war: Latest developments and international reactions":
@@ -54,7 +36,7 @@ FOX_HEADLINES = {
     ["War", "Russia-Ukraine", "World news"],
 
     "Supreme Court takes on major gun control case with national implications":
-    
+
     ["Supreme Court decisions", "Guns", "Justice system"]
 }
 
@@ -98,20 +80,6 @@ def generate_fake_subscribers(fake: Faker, num: int) -> list[tuple]:
     return subscribers
 
 
-def generate_fake_assignment(table1: list[tuple], table2: list[tuple], num_assignments: int) -> list[tuple]:
-    '''Generates random assignment table rows.
-    table1 and table2 are lists of rows in the mapped tables. 
-    It's assumed that the first item in each row is the id.'''
-
-    assignments = []
-    for i in range(1, num_assignments + 1):
-        subscriber = random.choice(table1)
-        topic = random.choice(table2)
-
-        assignments.append((i, subscriber[0], topic[0]))
-    return assignments
-
-
 def generate_fake_articles(faker: Faker, num_articles: int, source_id: int, source_headlines: list) -> list[tuple]:
     '''Generates fake article table entries.
     Returns a list of tuples containing:
@@ -127,8 +95,9 @@ def generate_fake_articles(faker: Faker, num_articles: int, source_id: int, sour
 
     for i in range(1, min(len(source_headlines), num_articles) + 1):
         article_id = i
+
         article_title = source_headlines[i-1]
-        source_id = 1
+        
         polarity_score = random.triangular(-1.0, 0.0, -0.5)
         date_published = faker.date_time_between(
             start_date='-5d')
