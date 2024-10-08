48dfe7e4b89863722f84276846f56c1de6245f2d
meganlester <trainee.megan.lester@sigmalabs.co.uk>
2024-10-08 17:27:56 +0100

    returns topics as dictionary

--- news_sentiment_analyser/openai_topics.py
+++ news_sentiment_analyser/openai_topics.py
@@ -5,10 +5,9 @@ import json
 
 
 def find_article_topics(article_titles: list[str], topics: list[str], openai_client) -> list[str]:
-    """Returns a list of topics associated with the article title, using openAI."""
-    content_topics = ", ".join(topics)
+    """Returns a list of topics associated with each article title, using openAI."""
     titles = "\n".join(article_titles)
-    system_content = f"Your job is to return a JSON object with the structure 'title': title, 'topics': list of topics (or empty list if none), for each article title. The topics are: {content_topics}"
+    system_content = create_message(topics)
     response = openai_client.chat.completions.create(
         messages=[
 
@@ -22,25 +21,18 @@ def find_article_topics(article_titles: list[str], topics: list[str], openai_cli
             }],
         model="gpt-4o-mini",
     )
-    return response.choices[0].message.content
+    response = json.loads(response.choices[0].message.content)
+    return response
 
 
-def create_message(article_title: str, topics: list[str]) -> str:
-    """Creates the message to be sent to the openAI."""
-    message_topics = "\n".join(topics)
-    message = f"""Given the news title: '{article_title}', 
-    identify which of the following topics it relates to:
-    \n\n{message_topics} \n\n
-    Please provide a list of matching topics in the format: 
-    'title: topic1, topic2'. or just 'title' if there are no matching topics."""
+def create_message(topics: list[str]) -> str:
+    """Creates the message to be sent to the openAI, as the system content."""
+    content_topics = ", ".join(topics)
+    message = f"""Your job is to return a JSON object with the structure 
+    'title': title, 'topics': list of topics (or empty list if none), 
+    for each article title. The topics are: {content_topics}"""
 
     return message
-
-
-def get_topics_from_response(response: str) -> list[str]:
-    """Returns the formatted list of topics from the openai response."""
-    if ":" not in response:
-        return []
 
 
 if __name__ == "__main__":
@@ -48,7 +40,4 @@ if __name__ == "__main__":
     ai = OpenAI(api_key=ENV["OPENAI_API_KEY"])
     results = find_article_topics(['AI is on the rise', 'Travelling to Columbia', 'Plane flights to the world cup'],
                                   ['Travel', 'Technology', 'Sports Events'], ai)
-    print(results)
-    print(type(results))
-    test = json.loads(results)
-    print(test)
+    print(results[0])
