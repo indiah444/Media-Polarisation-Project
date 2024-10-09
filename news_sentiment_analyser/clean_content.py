e907481753726bbcaa8765975ea32996d9198ba8
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-09 11:03:13 +0100

    added cleaning file and testing file

---
+++ news_sentiment_analyser/clean_content.py
@@ -0,0 +1,19 @@
+'''Cleaning HTML content'''
+
+import re
+
+
+def clean_html_tags(html_text: str) -> str:
+    '''Returns text with HTML tags removed'''
+
+
+def clean_multiple_spaces(text: str) -> str:
+    '''Returns text multiple spaces removed'''
+
+
+def clean_ads(article_content: str) -> str:
+    '''Returns article content with HTML Hyperlink text removed.'''
+
+
+if __name__ == "__main__":
+    ...
