9ff184fbacd9a8cb83758a3fa99889a145168dd6
Shayak Hussain <shayak201@gmail.com>
2024-10-09 18:20:23 +0100

    grab all news

--- democracy_now_scraper/extract_dn.py
+++ democracy_now_scraper/extract_dn.py
@@ -1,63 +1,27 @@
 """A file to extract information from various Fox News RSS feeds."""
 
+from datetime import datetime
+
 import grequests
-import feedparser
-import requests
 from bs4 import BeautifulSoup
 
 
-def fetch_rss_feed(feed_url: str) -> feedparser.FeedParserDict:
-    """Fetches and parsers the RSS feed from the provided URL."""
+def fetch_article_responses(links: list[str]) -> list:
+    """Fetches article responses asynchronously using grequests."""
 
-    feed = feedparser.parse(feed_url)
-    return feed
-
-
-def get_headline_title(div) -> str:
-    """Return title of headline given the headline div"""
-
-    title_tag = div.find("h2")
-    return title_tag.get_text(strip=True)
-
-
-def get_headline_contents(div) -> str:
-    """Return content of headline given the headline div"""
-
-    content_tags = div.find_all("p")
-    return '\n'.join([x.get_text(strip=True) for x in content_tags])
-
-
-def scrape_headlines(response) -> dict | str:
-    """
-    Get the titles and content of headlines 
-    from the Democracy Now headlines page
-    """
-
-    try:
-        soup = BeautifulSoup(response.content, "html.parser")
-        headline_divs = soup.find_all("div", class_="headline")
-
-        headlines = []
-        for div in headline_divs:
-            title = get_headline_title(div)
-            content = get_headline_contents(div)
-            headlines.append({"title": title, "content": content})
-
-        return headlines if headlines else "No headlines found"
-
-    except Exception as e:
-        return f"Failed to fetch content from {response.url}: {e}"
+    article_requests = (grequests.get(x, timeout=10) for x in links)
+    return grequests.map(article_requests)
 
 
 def get_article_title(soup) -> str:
-    """Return title of headline given the headline div"""
+    """Return title of article given the web body"""
 
     title_tag = soup.find("h1", class_="article__title")
     return title_tag.get_text(strip=True)
 
 
 def get_article_contents(soup) -> str:
-    """Return content of headline given the headline div"""
+    """Return content of article given the web body"""
 
     article_body = soup.find("div", class_="article__body")
     if not article_body:
@@ -67,9 +31,23 @@ def get_article_contents(soup) -> str:
     return "\n".join([p.get_text(strip=True) for p in paragraphs])
 
 
-def scrape_article(response: str) -> dict | str:
+def reformat_date(date: str):
+    """change from american date to standard YYYY-MM-DD"""
+
+    date_obj = datetime.strptime(date, "%B %d, %Y")
+
+    return date_obj.strftime("%Y-%m-%d")
+
+
+def get_article_date(soup) -> str:
+    """Return date of article given the web body"""
+
+    return reformat_date(soup.find("span", class_="date"))
+
+
+def scrape_article(response: str) -> dict:
     """
-    Get the title and main content of an article 
+    Get the title and main content of an article
     from Democracy Now article page.
     """
 
@@ -77,93 +55,94 @@ def scrape_article(response: str) -> dict | str:
         soup = BeautifulSoup(response.content, "html.parser")
         title = get_article_title(soup)
         content = get_article_contents(soup)
+        date = get_article_date(soup)
 
-        return {"title": title, "content": content}
+        return {"title": title, "content": content,
+                "link:": response.url, "published": date}
 
     except Exception as e:
 
         return f"Failed to fetch content from {response.url}: {e}"
 
 
-def fetch_article_responses(feed_entries: list) -> list:
-    """Fetches article responses asynchronously using grequests."""
+def get_all_topic_links() -> list[str]:
+    """Return links to all topics on the Democracy Now topics A-Z page."""
 
-    article_requests = (grequests.get(entry.link) for entry in feed_entries)
-    return grequests.map(article_requests)
+    try:
+        request = grequests.get("https://www.democracynow.org/topics/browse",
+                                timeout=10)
+        response = grequests.map([request])[0]
+        if response is None:
+            raise TypeError("Democracy now website not responding")
 
+        soup = BeautifulSoup(response.content, "html.parser")
+        topic_links = []
+        link_container = soup.find_all("div", class_="container-fluid")[4]
+        for li in link_container.find_all("li"):
+            link = li.find("a")
+            href = link.get("href")
+            if href:
+                topic_links.append(f"https://www.democracynow.org{href}")
 
-def process_headlines_entry(entry, response) -> list[dict] | str:
-    """Processes a headline entry and extracts its contents."""
+        return topic_links
 
-    page_link = entry.link
-    scraping_result = scrape_headlines(response)
-
-    if isinstance(scraping_result, list):
-        return [{"title": res["title"], "content": res["content"],
-                 "link": page_link, "published": entry.published}
-                for res in scraping_result]
-    else:
-        return scraping_result
-
-
-def process_article_entry(entry, response) -> dict | str:
-    """Processes an article entry and extracts its contents."""
-
-    content = scrape_article(response)
-
-    if isinstance(content, dict):
-        return {"title": entry.title, "content": content["content"],
-                "link": entry.link, "published": entry.published}
-    else:
-        return content
+    except Exception as e:
+        return f"Failed to fetch topic links: {e}"
 
 
-def handle_response(entry, response) -> list[dict] | None:
-    """Handles the response based on its status and content type."""
+def get_all_links_from_topic(topic_response: str) -> list[str]:
+    """Return links for a given topic page on Democracy Now."""
 
-    if response.status_code != 200:
-        print(f"Error code {response.status_code} for {entry.link}")
-        return None
-
-    if '/headlines' in entry.link:
-        return process_headlines_entry(entry, response)
-    else:
-        return [process_article_entry(entry, response)]
+    soup = BeautifulSoup(topic_response.content, "html.parser")
+    articles = soup.find_all(
+        "a", attrs={"data-ga-action": "Topic: Story Headline"})
+    article_links = [
+        f"https://www.democracynow.org{link.get('href')}" for link in articles if link.get("href")]
+    return article_links
 
 
-def parse_feed_entries(feed: feedparser.FeedParserDict) -> list[dict]:
+def get_all_links_from_all_topics() -> list[dict]:
+    """Scrape through all news from every DN topic"""
+
+    topic_links = get_all_topic_links()
+    if isinstance(topic_links, str):
+        return []
+    print(f"fetched all topics: {len(topic_links)}")
+    responses = fetch_article_responses(topic_links)
+    print(f"fetched all responses: {len(responses)}")
+    all_article_links = []
+    for i, response in enumerate(responses):
+        if response is None:
+            print(f"Could not connect to link: {topic_links[i]}")
+        else:
+            all_article_links.extend(get_all_links_from_topic(response))
+            print(f"extracted {len(responses)} links from {topic_links[i]}")
+
+    return all_article_links
+
+
+def parse_all_links(all_links: list[str]) -> list[dict]:
     """Parses the entries of a feed and extracts relevant fields."""
 
-    entries = []
-    responses = fetch_article_responses(feed.entries)
+    articles = []
+    responses = fetch_article_responses(all_links)
 
-    for entry, response in zip(feed.entries, responses):
-        processed_entries = handle_response(entry, response)
+    for response in responses:
+        articles.append(scrape_article(response))
 
-        if processed_entries is None:
-            continue
-        elif isinstance(processed_entries, list):
-            entries.extend(processed_entries)
-        else:
-            print(processed_entries)
-
-    return entries
+    return articles
 
 
-def fetch_from_multiple_feeds(feed_urls: list[str]) -> list[dict]:
-    """Fetches and parses entries from multiple RSS feeds."""
+def scrape_democracy_now():
+    """Scrape the democracy now pages to obtain all stories"""
 
-    all_entries = []
-    for url in feed_urls:
-        feed = fetch_rss_feed(url)
-        entries = parse_feed_entries(feed)
-        all_entries.extend(entries)
-
-    return all_entries
+    all_links = get_all_links_from_all_topics()
+    print(f"Retreived {len(all_links)} article links")
+    results = parse_all_links(all_links)
+    print(f"Extracted title and content from {len(all_links)} articles")
+    return results
 
 
 if __name__ == "__main__":
 
-    urls = ["https://www.democracynow.org/democracynow.rss"]
-    for res in fetch_from_multiple_feeds(urls):
-        print(res)
+    print(scrape_democracy_now())
