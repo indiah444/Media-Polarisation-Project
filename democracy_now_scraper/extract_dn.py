446b9a4dbd199867291117083cf5d1760a998890
Shayak Hussain <shayak201@gmail.com>
2024-10-09 19:10:35 +0100

    error handling

--- democracy_now_scraper/extract_dn.py
+++ democracy_now_scraper/extract_dn.py
@@ -1,6 +1,6 @@
 """A file to extract information from various Fox News RSS feeds."""
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import grequests
 from bs4 import BeautifulSoup
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 def fetch_article_responses(links: list[str]) -> list:
     """Fetches article responses asynchronously using grequests."""
 
-    article_requests = (grequests.get(x, timeout=10) for x in links)
+    article_requests = (grequests.get(x, timeout=30) for x in links)
     return grequests.map(article_requests)
 
 
@@ -17,6 +17,9 @@ def get_article_title(soup) -> str:
     """Return title of article given the web body"""
 
     title_tag = soup.find("h1", class_="article__title")
+    if not title_tag:
+        return None
+
     return title_tag.get_text(strip=True)
 
 
@@ -25,9 +28,12 @@ def get_article_contents(soup) -> str:
 
     article_body = soup.find("div", class_="article__body")
     if not article_body:
-        return "No Content Found"
+        return None
 
     paragraphs = article_body.find_all("p")
+    if not paragraphs:
+        return None
+
     return "\n".join([p.get_text(strip=True) for p in paragraphs])
 
 
@@ -42,7 +48,13 @@ def reformat_date(date: str):
 def get_article_date(soup) -> str:
     """Return date of article given the web body"""
 
-    return reformat_date(soup.find("span", class_="date"))
+    date = soup.find("span", class_="date")
+    if not date:
+        return None
+    try:
+        return reformat_date(date.get_text(strip=True))
+    except ValueError:
+        return None
 
 
 def scrape_article(response: str) -> dict:
@@ -70,7 +82,7 @@ def get_all_topic_links() -> list[str]:
 
     try:
         request = grequests.get("https://www.democracynow.org/topics/browse",
-                                timeout=10)
+                                timeout=30)
         response = grequests.map([request])[0]
         if response is None:
             raise TypeError("Democracy now website not responding")
@@ -105,41 +117,65 @@ def get_all_links_from_all_topics() -> list[dict]:
     """Scrape through all news from every DN topic"""
 
     topic_links = get_all_topic_links()
+    print(f"Found {len(topic_links)} topic links")
     if isinstance(topic_links, str):
         return []
-    print(f"fetched all topics: {len(topic_links)}")
-    responses = fetch_article_responses(topic_links)
-    print(f"fetched all responses: {len(responses)}")
+    responses = [x for x in fetch_article_responses(topic_links)
+                 if x is not None]
     all_article_links = []
     for i, response in enumerate(responses):
         if response is None:
             print(f"Could not connect to link: {topic_links[i]}")
         else:
             all_article_links.extend(get_all_links_from_topic(response))
-            print(f"extracted {len(responses)} links from {topic_links[i]}")
 
     return all_article_links
 
 
 def parse_all_links(all_links: list[str]) -> list[dict]:
-    """Parses the entries of a feed and extracts relevant fields."""
+    """Parses the contents of links listed."""
 
     articles = []
     responses = fetch_article_responses(all_links)
 
-    for response in responses:
-        articles.append(scrape_article(response))
-
+    for i, response in enumerate(responses):
+        if response is None:
+            print(f"Could not connect to url: {all_links[i]}")
+        else:
+            articles.append(scrape_article(response))
     return articles
+
+
+def link_is_old(link: str, time_diff: int) -> bool:
+    """
+    Filter out link that are more than 1 week old.
+    Expects links in the format 'https://www.democracynow.org/YYYY/M/D/...'.
+    """
+
+    one_week_ago = datetime.now() - timedelta(days=time_diff)
+
+    try:
+        date_str = '/'.join(link.split('/')[3:6])
+        link_date = datetime.strptime(date_str, "%Y/%m/%d")
+
+        if link_date >= one_week_ago:
+            return False
+
+    except ValueError as e:
+        # print(f"Skipping invalid link: {link}, error: {e}")
+        return True
+
+    return True
 
 
 def scrape_democracy_now():
     """Scrape the democracy now pages to obtain all stories"""
 
     all_links = get_all_links_from_all_topics()
-    print(f"Retreived {len(all_links)} article links")
-    results = parse_all_links(all_links)
-    print(f"Extracted title and content from {len(all_links)} articles")
+    all_valid_links = [x for x in all_links if not link_is_old(x, 7)]
+    print(f"Retreived {len(all_valid_links)} valid article links")
+    results = parse_all_links(all_valid_links)
+    print(f"Extracted title and content from {len(all_valid_links)} articles")
     return results
 
 
