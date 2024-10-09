bb0bb01902f0f6b310a57ad8cb16341b5ecb431e
India <india.howell123@gmail.com>
2024-10-09 13:49:10 +0100

    Updated script to include asynchronous calls to RSS feeds.

--- fox_news_scraper/extract_fn.py
+++ fox_news_scraper/extract_fn.py
@@ -1,6 +1,7 @@
 """A file to extract information from various Fox News RSS feeds."""
 
 import feedparser
+import grequests
 import requests
 from bs4 import BeautifulSoup
 
@@ -17,9 +18,12 @@ def get_article_content(article_url: str) -> str:
 
     try:
         response = requests.get(article_url, timeout=10)
-        soup = BeautifulSoup(response.content, "html.parser")
+        if response.status_code != 200:
+            return f"Couldn't connect to article, status_code: {response.status_code}"
 
+        soup = BeautifulSoup(response.content, "html.parser")
         article_body = soup.find("div", class_="article-body")
+
         if article_body:
             return article_body.get_text(separator=" ", strip=True)
 
@@ -49,20 +53,38 @@ def parse_feed_entries(feed: feedparser.FeedParserDict) -> list[dict]:
 def fetch_from_multiple_feeds(feed_urls: list[str]) -> list[dict]:
     """Fetches and parses entries from multiple RSS feeds."""
 
-    all_entries = []
+    async_list = []
     for url in feed_urls:
-        feed = fetch_rss_feed(url)
-        entries = parse_feed_entries(feed)
-        all_entries.extend(entries)
+        action_item = grequests.get(url)
+        async_list.append(action_item)
+
+    responses = grequests.map(async_list)
+
+    all_entries = []
+    for response in responses:
+        if response and response.status_code == 200:
+            feed = feedparser.parse(response.content)
+            entries = parse_feed_entries(feed)
+            all_entries.extend(entries)
+        else:
+            print(f"Failed to fetch feed from {url}, status_code: {
+                  response.status_code if response else "No response"}")
 
     return all_entries
 
 
 if __name__ == "__main__":
 
-    test_fn = fetch_rss_feed(
+    test_feed = fetch_rss_feed(
         "https://moxie.foxnews.com/google-publisher/politics.xml")
+    test_feed_urls = [
+        "https://moxie.foxnews.com/google-publisher/politics.xml",
+        "https://moxie.foxnews.com/google-publisher/world.xml",
+        "https://moxie.foxnews.com/google-publisher/science.xml"
+    ]
 
-    test_extract = parse_feed_entries(test_fn)
+    test_extract = parse_feed_entries(test_feed)
+    test_extract_multiple_feeds = fetch_from_multiple_feeds(test_feed_urls)
 
     print(test_extract)
+    print(test_extract_multiple_feeds)
