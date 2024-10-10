81b25ee1d37eb3248bb986bf109e2c735c089416
meganlester <trainee.megan.lester@sigmalabs.co.uk>
2024-10-10 13:07:33 +0100

    improved pylint

--- news_sentiment_analyser/sentiment_analysis.py
+++ news_sentiment_analyser/sentiment_analysis.py
@@ -41,39 +41,3 @@ def get_avg_sentiment(sentiments: pd.DataFrame, topic_col: str, source_col: str)
 
 if __name__ == "__main__":
     sentiment_analyser = SentimentIntensityAnalyzer()
-    fake_data = {
-        "title": [
-            "Trump Criticizes 2024 Election Process",
-            "Harris Advocates for Climate Change Action",
-            "Natural Disaster Strikes Coastal City",
-            "Supreme Court Rules on Abortion Laws"
-        ],
-        "content": [
-            "In a recent rally, Donald Trump h...",
-            "Vice President Kamala Harris urged the...",
-            "A devastating hurricane hit the...",
-            "The Supreme Court ruled on..."
-        ],
-        "source_name": ["Fox News", "Democracy Now!", "Democracy Now!", "Fox News"],
-        "published": [
-            datetime(2023, 8, 12),
-            datetime(2024, 3, 5),
-            datetime(2024, 1, 22),
-            datetime(2023, 10, 14)
-        ],
-        "link": [
-            "https://example.com/trump-2024-criticism",
-            "https://example.com/harris-climate-change",
-            "https://example.com/natural-disaster-coast",
-            "https://example.com/supreme-court-abortion"
-        ],
-        "topics": [
-            ['Donald Trump', '2024 Presidential Election'],
-            ['Kamala Harris', 'Climate Change'],
-            ['Natural Disaster'],
-            ['Abortion', 'Crime and Law Enforcement']
-        ]
-    }
-    fake_articles = pd.DataFrame(fake_data)
-    print(get_sentiments(sentiment_analyser,
-          fake_articles, 'content', 'topics', 'source_name'))
