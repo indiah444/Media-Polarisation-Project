919a6bfec41fa1d8c02edca304398a50ed3280d0
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-10 10:22:11 +0100

    added tests for pipeline_fn

--- fox_news_scraper/test_pipeline_fn.py
+++ fox_news_scraper/test_pipeline_fn.py
@@ -35,3 +35,39 @@ def test_lambda_handler_process_called_correctly(mock_process_rss, mock_rss_feed
     lambda_handler(event, context)
 
     mock_process_rss.assert_called_once_with(mock_rss_feed_urls)
+
+
+@patch('pipeline_fn.RSS_FEED_URLS')
+@patch('pipeline_fn.process_rss_feeds_and_upload')
+def test_lambda_handler_failure_status(mock_process_rss, mock_rss_feed_urls):
+    """Test lambda_handler failure case."""
+
+    mock_rss_feed_urls.return_value = [
+        "https://moxie.foxnews.com/google-publisher/latest.xml"]
+
+    mock_process_rss.side_effect = Exception()
+
+    event = {}
+    context = {}
+
+    response = lambda_handler(event, context)
+
+    assert response["statusCode"] == 500
+
+
+@patch('pipeline_fn.RSS_FEED_URLS')
+@patch('pipeline_fn.process_rss_feeds_and_upload')
+def test_lambda_handler_failure_error_message(mock_process_rss, mock_rss_feed_urls):
+    """Test lambda_handler failure case."""
+
+    mock_rss_feed_urls.return_value = [
+        "https://moxie.foxnews.com/google-publisher/latest.xml"]
+
+    mock_process_rss.side_effect = Exception("S3 upload failed")
+
+    event = {}
+    context = {}
+
+    response = lambda_handler(event, context)
+
+    assert "Error: S3 upload failed" in response["body"]
