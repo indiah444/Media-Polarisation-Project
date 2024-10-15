fa8bcc16548113c9bc81f2457f1e34e3ee309d6a
Shayak Hussain <shayak201@gmail.com>
2024-10-15 12:39:32 +0100

    standardise html_content.py

--- daily-emailing/html_content.py
+++ daily-emailing/html_content.py
@@ -1,4 +1,5 @@
 """Script to generate the html content."""
+
 from datetime import datetime, timedelta
 
 import pandas as pd
@@ -8,6 +9,7 @@ from d_db_funcs import get_yesterday_links
 
 def pivot_df(df: pd.DataFrame) -> pd.DataFrame:
     """Pivots dataframe so topics are rows and sources are columns."""
+
     pivoted_df = df.pivot(index='topic_name', columns='source_name',
                           values='avg_polarity_score').fillna('N/A')
     return pivoted_df
@@ -15,6 +17,7 @@ def pivot_df(df: pd.DataFrame) -> pd.DataFrame:
 
 def add_source_columns(df: pd.DataFrame) -> str:
     """Add the source names as column titles."""
+
     html = ""
     for source in df.columns:
         html += f"<th style='background-color: white;'>{source}</th>"
@@ -23,13 +26,15 @@ def add_source_columns(df: pd.DataFrame) -> str:
 
 def add_topic_rows(df: pd.DataFrame) -> str:
     """Build the rows of the table with topic and score, with color based on score."""
+
     html = ""
     for topic, row in df.iterrows():
         html += f"<tr><td style='background-color: white;'>{topic}</td>"
         for score in row:
             if isinstance(score, float):
                 color = "#fabbb7" if score < -0.5 else "#b6f7ae" if score > 0.5 else "#fafafa"
-                html += f"<td style='background-color: {color};'>{score:.2f}</td>"
+                html += f"<td style='background-color: {
+                    color};'>{score:.2f}</td>"
             else:
                 html += f"<td style='background-color: white;'>{score}</td>"
         html += "</tr>"
@@ -38,6 +43,7 @@ def add_topic_rows(df: pd.DataFrame) -> str:
 
 def generate_html_with_links() -> str:
     """Creates a list of yesterdays articles urls."""
+
     urls = get_yesterday_links()
     html = "<h2>Yesterday's articles:</h2>\n<ul>\n"
     for url in urls:
@@ -48,6 +54,8 @@ def generate_html_with_links() -> str:
 
 
 def generate_html(df) -> str:
+    """Return HTML string to send in email body"""
+
     score_df = pivot_df(df)
     yesterday = (datetime.now() - timedelta(days=1)).strftime('%d-%m-%Y')
     html = """
