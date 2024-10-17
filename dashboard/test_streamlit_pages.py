f4ed1acdf1ca034474797e4b8f4dcdcf92dd230a
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-17 15:36:07 +0100

    added test

--- dashboard/test_streamlit_pages.py
+++ dashboard/test_streamlit_pages.py
@@ -5,12 +5,21 @@ from streamlit.testing.v1.app_test import AppTest
 
 
 def test_subscribe_elements_submission_page():
+    """Tests the presence of the subscribe button"""
     at = AppTest.from_file("pages/6_Subscribe.py")
     at.run()
     assert at.button[0].label == "Submit"
 
 
 def test_unsubscribe_elements_submission_page():
+    """Tests the presence of the unsubscribe button"""
     at = AppTest.from_file("pages/6_Subscribe.py")
+    at.run()
+    assert at.button[1].label == "Unsubscribe"
+
+
+def test_home_page():
+    """Tests that the home page runs without exception"""
+    at = AppTest.from_file("pages/2_About.py")
     at.run()
     assert at.button[1].label == "Unsubscribe"
