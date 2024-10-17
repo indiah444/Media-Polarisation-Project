c10cbd9cfe3430d6842aabc8b6bdfba6d20d0a87
ayahusseini <trainee.aya.husseini@sigmalabs.co.uk>
2024-10-17 15:54:07 +0100

    deleted unneeded function

--- dashboard/dataframe_functions.py
+++ dashboard/dataframe_functions.py
@@ -21,6 +21,7 @@ def is_valid_time_interval(interval: str) -> None:
 def resample_dataframe(df: pd.DataFrame, time_interval: str, aggregate: str):
     """Resamples the dataframe to return the aggregate sentiment scores by 
     (source, topic) over a set of grouped time intervals."""
+
     if not aggregate in AGGREGATES:
         raise ValueError(
             f"The aggregate parameter must be one of {AGGREGATES}.")
@@ -31,7 +32,7 @@ def resample_dataframe(df: pd.DataFrame, time_interval: str, aggregate: str):
         time_interval, on='date_published').agg({"title_polarity_score": aggregate,
                                                  "content_polarity_score": aggregate}).reset_index()
 
-    return pd.DataFrame(df_avg)
+    return df_avg
 
 
 def add_year_month_day_columns(data_df: pd.DataFrame) -> pd.DataFrame:
